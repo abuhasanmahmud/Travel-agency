d5dafbe16fe1a309033338d6852ace2c64fb4163
abuhasanmahmud <mahmud.me420@gmail.com>
2023-11-24 19:54:08 +0600

    F

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -11,7 +11,7 @@ import React from "react";
 
 const Category = () => {
   console.log("category");
-  // console.log("ss");
+  console.log("ss");
   return (
     <div className="bg-slate-100 md:py-14 py-10  ">
       <div className="text-center pb-5">
