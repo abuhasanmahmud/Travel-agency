a5d99871afa41492a33c18fcacf784504d4485bc
abuhasanmahmud <mahmud.me420@gmail.com>
2023-12-06 18:33:44 +0600

    s

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,7 +5,7 @@ import Testimonial from "../testimonial/Testimonial";
 import TravelLogo from "../travel&logo/TravelLogo";
 
 const Home = () => {
-  // console.log("ss");
+  console.log("ss");
   // console.log("ss");
   return (
     <>
